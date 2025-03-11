e59cd17e3cf39a9d48f7ba4e89348bd99663f1e3
Eugene ZenBerry <eugeneberezhnoy@gmail.com>
2025-03-12 01:34:37 +0200

    Todo

--- background.js
+++ background.js
@@ -1,11 +1,1 @@
-// Will be used to open the settings in a new tab
-
-// // Handle the browser action (clicking on the plugin's icon)
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   openOptionsPage(); // Open the options page in a new tab
-// });
-
-// // Function to open the options page in a new tab
-// function openOptionsPage() {
-//   chrome.tabs.create({ url: chrome.runtime.getURL("options.html") });
-// }
+// TODO
